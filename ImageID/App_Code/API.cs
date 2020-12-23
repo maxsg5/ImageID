989dd16366768694439c9612a3b900692e83ddc9
Max Schafer <maxsg5@hotmail.com>
2020-12-23 13:36:09 -0700

    call to API

--- ImageID/App_Code/API.cs
+++ ImageID/App_Code/API.cs
@@ -11,23 +11,23 @@ namespace ImageID.App_Code
 {
     public class API
     {
-        public static void CallAPI()
+        public static string CallAPI()
         {
             // Configure API key authorization: Apikey
             Configuration.Default.AddApiKey("Apikey", "4f84b15b-62fb-4d35-9014-92ef67496abe");
 
             var apiInstance = new RecognizeApi();
-            var imageFile = new System.IO.FileStream("C:\\temp\\inputfile", System.IO.FileMode.Open); // System.IO.Stream | Image file to perform the operation on.  Common file formats such as PNG, JPEG are supported.
+            var imageFile = new System.IO.FileStream("C:\\Users\\Maxsg\\source\\repos\\ImageID\\ImageID\\Images\\image1.jpg", System.IO.FileMode.Open); // System.IO.Stream | Image file to perform the operation on.  Common file formats such as PNG, JPEG are supported.
 
             try
             {
                 // Describe an image in natural language
                 ImageDescriptionResponse result = apiInstance.RecognizeDescribe(imageFile);
-                Debug.WriteLine(result);
+                return result.ToString();
             }
             catch (Exception e)
             {
-                Debug.Print("Exception when calling RecognizeApi.RecognizeDescribe: " + e.Message);
+                return "Exception when calling RecognizeApi.RecognizeDescribe: " + e.Message;
             }
         }
     }
--- ImageID/Main.aspx.cs
+++ ImageID/Main.aspx.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ImageID.App_Code;
 
 namespace ImageID
 {
@@ -16,7 +17,6 @@ namespace ImageID
 
         protected void btnUpload_Click(object sender, EventArgs e)
         {
-
             if (!fu.HasFile)
             {
                 lblStatus.Text = "No File Selected";
@@ -29,13 +29,12 @@ namespace ImageID
                 return;
             }
 
-            //MapPath() to construct a full path into your Uploads directory appending his UserName. 
-            string destDir = MapPath("/Images"); // destination directory 
+            //MapPath() to construct a full path into your Uploads directory appending his UserName.
+            string destDir = MapPath("/Images"); // destination directory
             string fileName = fu.FileName; // user's file name !! careful..
 
             // NOTE : file name is your APP's choice ! You Pick
-            string savePath = destDir + @"\" + fileName;
-
+            string savePath = destDir + @"\image1.jpg";
 
             //see if directory exists, if not, create it
             try
@@ -50,11 +49,10 @@ namespace ImageID
                 {
                     if (Directory.GetFiles(destDir).Length == 0) //no files, then save
                     {
-
                         try
                         {
-
                             fu.SaveAs(savePath);
+                            lblStatus.Text = API.CallAPI();
                         }
                         catch (Exception exc)
                         {
@@ -64,15 +62,11 @@ namespace ImageID
                     }
                     else //there's files, deelete it
                     {
-
-                        
-                        
-                            
-                        
                         try
                         {
                             File.Delete(Directory.GetFiles(destDir)[0]);
                             fu.SaveAs(savePath);
+                            lblStatus.Text = API.CallAPI();
                         }
                         catch (Exception exc)
                         {
@@ -81,15 +75,11 @@ namespace ImageID
                         }
                     }
                 }
-
-
             }
             catch (Exception exception)
             {
                 lblStatus.Text = $"The process failed: {exception}";
             }
-
-
 
             FileInfo fi = new FileInfo(savePath);
 
